9acfc6f06711fcdfca299435f43a6d2c0450ad03
Aeron Greva <ultrasonicaeron@gmail.com>
2019-12-18 20:35:31 -0500

    Scripted Eclipse doesn't work

--- scripts/eclipseScript.js
+++ scripts/eclipseScript.js
@@ -1,3 +1,4 @@
+//This doesn't work at the moment. Just throws an error.
 const scriptedEclipse = extendContent(FlyingUnit, "eclipse-scripted", {
 	update() {
 		print("Aw fuck, I can't believe you've done this.");
