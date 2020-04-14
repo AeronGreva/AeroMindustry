ab077bd6c30d4996cf119ce2b8aef1ebf6c45447
Aeron Greva <ultrasonicaeron@gmail.com>
2020-04-14 17:44:46 -0400

    Moved all content names and descriptions to the English bundle

--- [unused things]/scripts/eclipseScript.js
+++ [unused things]/scripts/eclipseScript.js
@@ -1,6 +1,6 @@
-//This doesn't work at the moment. Just throws an error.
+//This was once going to be used to make a custom death animation for the Eclipse. As you can see, that never went anywhere.
 const scriptedEclipse = extendContent(FlyingUnit, "eclipse-scripted", {
 	update() {
-		print("Aw fuck, I can't believe you've done this.");
+		print("[AeMin]Aw fuck, I can't believe you've done this.");
 	}
 });
