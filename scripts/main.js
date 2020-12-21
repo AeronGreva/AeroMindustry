b0e193aaf9504dba66e3087afc4f09b3a80dced9
Aeron Greva <ultrasonicaeron@gmail.com>
2020-12-21 16:49:12 -0500

    Reintroduced a small handful of content

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,6 @@
-print("[AeMin] AeroMindustry has been loaded!");
 // require("energy-shield");
+
+Events.on(ContentInitEvent, () => {
+	print("[AeMin] AeroMindustry has been loaded!");
+	});
+//To do: Find a way to make ObjectMaps. We can supply the necessary objects.
